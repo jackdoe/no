9a56aa152c166f1ca538dfc8a9a2cf3c119aa496
Rares Mirica <rares.mirica@booking.com>
2015-11-27 10:51:00 +0100

    Reformat source

--- ensamble/member.js
+++ ensamble/member.js
@@ -5,113 +5,116 @@ var protobuf = require('protocol-buffers')
 var path = require('path');
 var messages = protobuf(fs.readFileSync(path.resolve(__dirname, 'data.proto')));
 
-const util = require('util');
-const EventEmitter = require('events');
+const
+util = require('util');
+const
+EventEmitter = require('events');
 
 var protocol_decoder = require('./protocol_decoder.js');
 
 function Member(node, client) {
-	
-	this.node = node;
-	this.alive = false;
-	this.client = undefined;
-	
-	if(client) {
-		this.init_client(this, client);
-	} else {
-		this.open_connection();
-	}
-	
-	EventEmitter.call(this);
+
+    this.node = node;
+    this.alive = false;
+    this.client = undefined;
+
+    if (client) {
+        this.init_client(this, client);
+    } else {
+        this.open_connection();
+    }
+
+    EventEmitter.call(this);
 }
 
 util.inherits(Member, EventEmitter);
 
 Member.prototype.getName = function() {
-	return this.node;
+    return this.node;
 }
 
 Member.prototype.init_client = function(me, client) {
-	var me = me;
-	
 
-	client.on('close', function() {
-		console.log("seding bye!");
-		me.emit('bye');
-	});
-	
-	client.on('decoded_message', me.onMessage.bind(me));
+    client.on('close', function() {
+        console.log("seding bye!");
+        me.emit('bye');
+    });
 
-	me.name = client.name
-	me.client = client;
+    client.on('decoded_message', me.onMessage.bind(me));
+
+    me.name = client.name
+    me.client = client;
 }
 
 Member.prototype.onMessage = function(message, socket) {
-	console.log("I got a decoded message from node: " + this.node);
-	
-	this.emit(protocol_decoder.message_types[message.header.type], message);
-	
-	console.log(message);
+    console.log("I got a decoded message from node: " + this.node);
+
+    this.emit(protocol_decoder.message_types[message.header.type], message);
+
+    console.log(message);
 }
 
 Member.prototype.open_connection = function() {
-	console.log("Opening connection to: " + this.node);
-	
-	var me = this;
-	var up = url.parse(this.node);
-	console.dir(up);
-	
-	var client = net.connect({host: up.hostname, port: up.port},
-	  function() { //'connect' listener
-		  console.log('connected to server!');
-		  
-	});
+    console.log("Opening connection to: " + this.node);
 
-	client.on('connect', function(conn) {
-		console.log('connection establ');
-		me.init_client(me, client);
-		protocol_decoder.decode_socket(client);
+    var me = this;
+    var up = url.parse(this.node);
+    console.dir(up);
 
-		this.send("HELLO");
-		
-		}.bind(this));
-	
-	client.on('error', function( err ) {
-		console.log("ERROR!!!")
-		console.dir(err);
-	});
+    var client = net.connect({
+        host : up.hostname,
+        port : up.port
+    }, function() {
+        // 'connect' listener
+        console.log('connected to server!');
+    });
+
+    client.on('connect', function(conn) {
+        console.log('connection establ');
+        me.init_client(me, client);
+        protocol_decoder.decode_socket(client);
+
+        this.send("HELLO");
+
+    }.bind(this));
+
+    client.on('error', function(err) {
+        console.log("ERROR!!!")
+        console.dir(err);
+    });
 }
 
 Member.prototype.send = function(type, payload) {
 
-	var data = {
-		'header': {
-			'node_id': 6,
-			'type': messages.Ensamble_Header.Type[type]
-		}
-	};
-	
-	if(payload != undefined) {
-		data["payload"] = {"data": payload};
-	}
-	
-	var encoded_data = messages.Ensamble_Message.encode(data);
+    var data = {
+        'header' : {
+            'node_id' : 6,
+            'type' : messages.Ensamble_Header.Type[type]
+        }
+    };
 
-	var blen = new Buffer(4);
-	blen.fill(0);
-	blen.writeUInt32BE(encoded_data.length, 0);
+    if (payload != undefined) {
+        data["payload"] = {
+            "data" : payload
+        };
+    }
 
-	var send_buffer = Buffer.concat([blen, encoded_data]);
+    var encoded_data = messages.Ensamble_Message.encode(data);
 
-	this.client.write(send_buffer, function() {
-		console.log("I have sent my message of type: " + type);
-	});
+    var blen = new Buffer(4);
+    blen.fill(0);
+    blen.writeUInt32BE(encoded_data.length, 0);
+
+    var send_buffer = Buffer.concat([ blen, encoded_data ]);
+
+    this.client.write(send_buffer, function() {
+        console.log("I have sent my message of type: " + type);
+    });
 
 }
 
 Member.prototype.stop = function() {
-	console.log("Stop member: " + this.host);
+    console.log("Stop member: " + this.host);
 }
-
 
 module.exports = Member;
--- rw.js
+++ rw.js
@@ -23,20 +23,19 @@ var argv = require('minimist')(process.argv.slice(2));
 
 ensamble.start(parseInt(argv.intercom));
 
-if(argv.ensamble) {
-	ensamble.reset();
-	
-	var ensamble_members = argv.ensamble.split(",");
-	ensamble_members.forEach(function(m) {
-		
-		ensamble.addMember("rw://" + m);
-	});
-	
-	ensamble.save();
-} else {
-	ensamble.load();
-}
+if (argv.ensamble) {
+    ensamble.reset();
 
+    var ensamble_members = argv.ensamble.split(",");
+    ensamble_members.forEach(function(m) {
+
+        ensamble.addMember("rw://" + m);
+    });
+
+    ensamble.save();
+} else {
+    ensamble.load();
+}
 
 var WCOUNTER = 0;
 var RCOUNTER = 0;
@@ -47,26 +46,30 @@ var WRITER_PORT = argv.writer || 8001;
 var NODE_ID = argv.node_id || 0;
 var WRITER_UDP_PORT = argv.udp || 0;
 var SEARCHER_PORT = argv.searcher || 8002;
-var POOL = (argv.pool instanceof Array ? argv.pool : [argv.pool] )
-    .filter(function(e) { return e })
-    .map(function(e) {
-        if (!e.startsWith("http://"))
-            e = "http://" + e;
+var POOL = (argv.pool instanceof Array ? argv.pool : [ argv.pool ]).filter(
+        function(e) {
+            return e
+        }).map(function(e) {
+    if (!e.startsWith("http://"))
+        e = "http://" + e;
 
-        var u = url.parse(e)
-        return { host: u.hostname, port: u.port || WRITER_UDP_PORT }
-    });
+    var u = url.parse(e)
+    return {
+        host : u.hostname,
+        port : u.port || WRITER_UDP_PORT
+    }
+});
 
 var Store = require('./store.js')(ROOT, NODE_ID);
 
 var TERMINATED = new Buffer(1);
 TERMINATED.fill(0);
 
-Array.prototype.random = function () {
-  return this[Math.floor((Math.random()*this.length))];
+Array.prototype.random = function() {
+    return this[Math.floor((Math.random() * this.length))];
 }
 
-var fn_for_tag = function(time_id,tag) {
+var fn_for_tag = function(time_id, tag) {
     return ROOT + time_id + '/tag#' + tag + '.txt';
 }
 
@@ -78,7 +81,7 @@ var get_store_obj = function(time_id, cache) {
     return cache[time_id];
 }
 
-var ts_to_id = function (ts) {
+var ts_to_id = function(ts) {
     return Math.floor(ts);
 }
 var time = function() {
@@ -128,7 +131,7 @@ function DocumentIdentifier() {
 
     this.to_string = function() {
         if (this.no_more())
-            return  "NO_MORE";
+            return "NO_MORE";
         if (this.time_id == -1)
             return "NO_INIT";
         return "time_id: " + this.time_id + ",offset: " + this.offset;
@@ -148,13 +151,15 @@ function Term(tag) {
     this.size = 0;
     this.not_initialized = true;
 
-    this.set_time_id_range = function(from,to) {
+    this.set_time_id_range = function(from, to) {
         this.from = parseInt(from) || time_dec(time());
         this.time_id = this.from;
         this.to = to ? parseInt(to) : time();
     }
 
-    this.left = function() { return this.size - this.offset };
+    this.left = function() {
+        return this.size - this.offset
+    };
 
     this.jump = function(to_doc_id) {
         if (this.doc_id.equals(to_doc_id))
@@ -174,7 +179,7 @@ function Term(tag) {
 
     this.pick_closest_non_zero_file = function(temp_time_id) {
         var end = this.to;
-        while(temp_time_id <= end) {
+        while (temp_time_id <= end) {
             if (this.open_time_id(temp_time_id))
                 return true;
             temp_time_id = time_inc(temp_time_id);
@@ -213,20 +218,24 @@ function Term(tag) {
         return this.pick_closest_non_zero_file(time_inc(this.time_id));
     }
 
-    this.next = function () {
+    this.next = function() {
         if (this.doc_id.no_more())
             return this.doc_id;
         if (this.reopen_if_needed()) {
             if (this.left() >= 6) {
                 this.buffer.fill(0);
 
-                var n_read = fs.readSync(this.fd,this.buffer,0,6,this.offset);
-                if (n_read != 6) throw(new Error("failed to read 6 bytes, got:" + n_read + " size: " + size + " at offset: " + this.offset));
+                var n_read = fs.readSync(this.fd, this.buffer, 0, 6,
+                        this.offset);
+                if (n_read != 6)
+                    throw (new Error("failed to read 6 bytes, got:" + n_read
+                            + " size: " + size + " at offset: " + this.offset));
 
                 this.offset += 6;
                 this.doc_id.time_id = this.time_id;
 
-                this.doc_id.offset = (this.buffer.readUInt32BE(2) << 16) | this.buffer.readUInt16BE(0);
+                this.doc_id.offset = (this.buffer.readUInt32BE(2) << 16)
+                        | this.buffer.readUInt16BE(0);
                 return this.doc_id;
             }
         }
@@ -234,13 +243,14 @@ function Term(tag) {
         this.doc_id.reset();
         return this.doc_id;
     }
-    this.to_string = function () {
-        return "tag:" + this.tag + "@" + (this.from || 0) + ":" + (this.to || 0) + "#" + this.doc_id.to_string();
+    this.to_string = function() {
+        return "tag:" + this.tag + "@" + (this.from || 0) + ":"
+                + (this.to || 0) + "#" + this.doc_id.to_string();
     }
 }
 
 function TermOffsetTimeId(list) {
-    this.list = list.sort(function(a,b) {
+    this.list = list.sort(function(a, b) {
         var v = parseInt(a.time_id) - parseInt(b.time_id);
         if (v != 0)
             return v;
@@ -249,7 +259,7 @@ function TermOffsetTimeId(list) {
 
     this.doc_id = new DocumentIdentifier();
     this.cursor = -1;
-    this.set_time_id_range = function(from,to) {
+    this.set_time_id_range = function(from, to) {
         // XXX: does it make sense to honor from/to here?
     }
 
@@ -271,7 +281,7 @@ function TermOffsetTimeId(list) {
         return this.doc_id;
     }
 
-    this.next = function () {
+    this.next = function() {
         if (this.doc_id.no_more())
             return this.doc_id;
 
@@ -285,8 +295,9 @@ function TermOffsetTimeId(list) {
         return this.doc_id;
     }
 
-    this.to_string = function () {
-        return "{list:" + JSON.stringify(this.list) + "@" + (this.from || 0) + ":" + (this.to || 0)+ "#" + this.doc_id.to_string() + "}";
+    this.to_string = function() {
+        return "{list:" + JSON.stringify(this.list) + "@" + (this.from || 0)
+                + ":" + (this.to || 0) + "#" + this.doc_id.to_string() + "}";
     }
 }
 
@@ -305,7 +316,7 @@ function BoolOr() {
             return this.doc_id;
         }
 
-        while(true) {
+        while (true) {
             if (this.doc_id.no_more())
                 return this.doc_id;
 
@@ -319,12 +330,12 @@ function BoolOr() {
         }
     }
 
-    this.set_time_id_range = function(from,to) {
-        for(var i = 0; i < this.queries.length; i++)
-            this.queries[i].set_time_id_range(from,to);
+    this.set_time_id_range = function(from, to) {
+        for (var i = 0; i < this.queries.length; i++)
+            this.queries[i].set_time_id_range(from, to);
     }
 
-    this.next = function () {
+    this.next = function() {
         if (this.doc_id.no_more())
             return this.doc_id;
 
@@ -341,8 +352,10 @@ function BoolOr() {
         return this.doc_id;
     }
 
-    this.to_string = function () {
-        return "{OR(" + this.queries.map(function(e) { return e.to_string() }).join(",") + ")" + "#" + this.doc_id.to_string() +"}";
+    this.to_string = function() {
+        return "{OR(" + this.queries.map(function(e) {
+            return e.to_string()
+        }).join(",") + ")" + "#" + this.doc_id.to_string() + "}";
     }
 }
 
@@ -354,19 +367,19 @@ function BoolAnd() {
         this.or.add(query);
     }
 
-    this.set_time_id_range = function(from,to) {
-        this.or.set_time_id_range(from,to);
+    this.set_time_id_range = function(from, to) {
+        this.or.set_time_id_range(from, to);
     }
 
     this.jump = function(to_doc_id) {
         return this.next_with_target(this.or.jump(to_doc_id));
     }
 
-    this.next = function () {
+    this.next = function() {
         return this.next_with_target(this.or.next());
     }
 
-    this.next_with_target = function (to_doc_id) {
+    this.next_with_target = function(to_doc_id) {
         if (to_doc_id.no_more()) {
             this.doc_id.reset();
             return this.doc_id;
@@ -376,7 +389,7 @@ function BoolAnd() {
             return this.doc_id;
 
         var new_doc = to_doc_id;
-        while(true) {
+        while (true) {
             if (new_doc.no_more()) {
                 this.doc_id.set(new_doc);
                 return this.doc_id;
@@ -400,8 +413,9 @@ function BoolAnd() {
         }
     }
 
-    this.to_string = function () {
-        return "{AND[" + this.or.to_string() +"]" + "#" + this.doc_id.to_string() +"}";
+    this.to_string = function() {
+        return "{AND[" + this.or.to_string() + "]" + "#"
+                + this.doc_id.to_string() + "}";
     }
 }
 
@@ -413,7 +427,7 @@ var parse = function(obj) {
         q = new TermOffsetTimeId(obj.list);
     } else {
         if (!obj.and && !obj.or)
-            throw(new Error("dont know what to do with " + JSON.stringify(obj)));
+            throw (new Error("dont know what to do with " + JSON.stringify(obj)));
 
         var arr = obj.and ? obj.and : obj.or;
         if (arr.length == 1) {
@@ -440,21 +454,23 @@ var err_handler = function(response, e, interval, do_not_terminate) {
     console.log(msg);
 };
 
-var searcher = http.createServer(function (request, response) {
+var searcher = http.createServer(function(request, response) {
     response.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache',
+        'Content-Type' : 'text/event-stream',
+        'Connection' : 'keep-alive',
+        'Cache-Control' : 'no-cache',
     });
     var url_parts = url.parse(request.url, true);
     var qs = url_parts.query;
 
     var body = '';
-    request.on('data', function (data) { body += data; });
+    request.on('data', function(data) {
+        body += data;
+    });
 
     var cache = {};
     var cleanup = function() {
-        for (var k in cache) {
+        for ( var k in cache) {
             cache[k].cleanup();
             delete cache[k];
         }
@@ -464,7 +480,7 @@ var searcher = http.createServer(function (request, response) {
         if (qs.sub) {
             var sub = qs.sub;
             if (!(sub instanceof Array))
-                sub = [sub];
+                sub = [ sub ];
             var frames = [];
             var decoded = messages.Data.decode(bytes);
             var seen = {}
@@ -478,8 +494,8 @@ var searcher = http.createServer(function (request, response) {
             }
 
             var output = {
-                header: decoded.header,
-                frames: frames
+                header : decoded.header,
+                frames : frames
             };
             bytes = messages.Data.encode(output);
         }
@@ -495,36 +511,45 @@ var searcher = http.createServer(function (request, response) {
         response.write(bytes, 'binary');
     };
 
-    request.on('end', function () {
+    request.on('end', function() {
         try {
             obj = JSON.parse(body);
             var q = parse(obj);
             var n;
-            while(true) {
+            while (true) {
                 var n = q.next()
                 if (n.no_more()) {
                     response.end();
                     cleanup();
                     break;
                 }
-                send(get_store_obj(n.time_id,cache).get(n.offset));
-            };
-        } catch(e) {
-            err_handler(response,e,undefined);
-        };
-        response.on('error', function() { cleanup() });
-        response.on('end', function() { cleanup() });
+                send(get_store_obj(n.time_id, cache).get(n.offset));
+            }
+            ;
+        } catch (e) {
+            err_handler(response, e, undefined);
+        }
+        ;
+        response.on('error', function() {
+            cleanup()
+        });
+        response.on('end', function() {
+            cleanup()
+        });
     });
-    request.connection.on('close',function(){ cleanup() });
-
+    request.connection.on('close', function() {
+        cleanup()
+    });
 
 });
 
-var acceptor = http.createServer(function (request, response) {
+var acceptor = http.createServer(function(request, response) {
     var url_parts = url.parse(request.url, true);
     var body = new Buffer(0);
-    request.on('data', function (data) { body = Buffer.concat([body,data]) });
-    request.on('end', function () {
+    request.on('data', function(data) {
+        body = Buffer.concat([ body, data ])
+    });
+    request.on('end', function() {
         try {
             var decoded = messages.Data.decode(body)
             var t = time();
@@ -532,15 +557,20 @@ var acceptor = http.createServer(function (request, response) {
             var encoded = s.append(decoded);
             WCOUNTER++;
 
-            response.writeHead(200, {"Content-Type": "application/json"});
-            response.end(JSON.stringify({offset: s.position, fn: s.time_id}));
+            response.writeHead(200, {
+                "Content-Type" : "application/json"
+            });
+            response.end(JSON.stringify({
+                offset : s.position,
+                fn : s.time_id
+            }));
         } catch (e) {
             err_handler(response, e, undefined, true);
         }
     });
 });
 
-process.on('uncaughtException', function(e){
+process.on('uncaughtException', function(e) {
     console.log((e instanceof Error ? e.stack : e));
 });
 
@@ -551,15 +581,22 @@ if (SEARCHER_PORT > 0)
     searcher.listen(SEARCHER_PORT);
 
 if (WRITER_UDP_PORT > 0) {
-    udp.on('message', function (message, remote) { WCOUNTER++; get_store_obj(time(), NAME_TO_STORE).append(messages.Data.decode(message), function() {}); });
+    udp.on('message', function(message, remote) {
+        WCOUNTER++;
+        get_store_obj(time(), NAME_TO_STORE).append(
+                messages.Data.decode(message), function() {
+                });
+    });
     udp.bind(WRITER_UDP_PORT);
 }
 
-console.log("running on writer: http@" + WRITER_PORT + "/udp@" + WRITER_UDP_PORT +", searcher: http@" + SEARCHER_PORT + " POOL: " + JSON.stringify(POOL) + " NODE_ID: " + NODE_ID);
+console.log("running on writer: http@" + WRITER_PORT + "/udp@"
+        + WRITER_UDP_PORT + ", searcher: http@" + SEARCHER_PORT + " POOL: "
+        + JSON.stringify(POOL) + " NODE_ID: " + NODE_ID);
 setInterval(function() {
     cleaned = 0;
     // NAME_TO_STORE is only used for writers
-    for (var k in NAME_TO_STORE) {
+    for ( var k in NAME_TO_STORE) {
         if (k < time_dec(time())) {
             console.log("cleaning up: " + k);
             NAME_TO_STORE[k].cleanup();
@@ -567,10 +604,10 @@ setInterval(function() {
             cleaned++;
         }
     }
-    console.log(time() + " written: " + WCOUNTER + "/s, searched: " + RCOUNTER + "/s, cleaned: " + cleaned);
+    console.log(time() + " written: " + WCOUNTER + "/s, searched: " + RCOUNTER
+            + "/s, cleaned: " + cleaned);
     RCOUNTER = 0;
     WCOUNTER = 0;
-    
-    
+
     console.log(Object.keys(ensamble.getMembers()));
-},1000);
+}, 1000);
